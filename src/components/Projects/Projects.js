72117c050bde0ee9ba423f36ad8b9d294e4f3105
Armando Martinez <alemar.martinez16@gmail.com>
2023-08-01 00:16:07 -0300

    Proyects.js dinalizado + imagenes

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -4,10 +4,10 @@ import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
 import leaf from "../../Assets/Projects/leaf.png";
 import emotion from "../../Assets/Projects/emotion.png";
-import editor from "../../Assets/Projects/codeEditor.png";
-import chatify from "../../Assets/Projects/chatify.png";
+import imagen_proyect3 from "../../Assets/Projects/Captura_Rick.jpeg";
+import imagen_proyect1 from "../../Assets/Projects/Captura_Home.png";
 import suicide from "../../Assets/Projects/suicide.png";
-import bitsOfCode from "../../Assets/Projects/blog.png";
+import imagen_proyect2 from "../../Assets/Projects/Captura_Tasty.png";
 
 function Projects() {
   return (
@@ -23,68 +23,34 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={chatify}
+              imgPath={imagen_proyect1}
               isBlog={false}
-              title="Chatify"
-              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
-              ghLink="https://github.com/soumyajit4419/Chatify"
-              demoLink="https://chatify-49.web.app/"
+              title="El Rincón del Sabiondo"
+              description="E-commerce with design and development of the following features: CRUD of products, user registration and authentication, catalog, checkout, payment gateway (MercadoPago), sending of transactional emails and newsletters, shopping cart, management of stock and purchase orders, administration of users and roles, review and rating of purchased products."
+              ghLink="https://github.com/Marifuhr/el_rincon_del_sabiondo"
+              demoLink="https://el-rincon-del-sabiondo-eta.vercel.app/"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={bitsOfCode}
+              imgPath={imagen_proyect2}
               isBlog={false}
-              title="Bits-0f-C0de"
-              description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
-              ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
-              demoLink="https://blogs.soumya-jit.tech/"
+              title="Tasty Cooking"
+              description="Tasty Cooking is a personal project I developed using the knowledge I acquired during my bootcamp at Henry. The goal of the project was to create a cooking app using modern and efficient technologies like React, Redux, Node and Sequelize. The project uses the Spoonacular API to consume recipe information, which has allowed me to enrich the app's database and offer users a more complete experience."
+              ghLink="https://github.com/Alemar16/Tasty-Cooking"
+              demoLink=" "
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={editor}
+              imgPath={imagen_proyect3}
               isBlog={false}
-              title="Editor.io"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              ghLink="https://github.com/soumyajit4419/Editor.io"
-              demoLink="https://editor.soumya-jit.tech/"              
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={leaf}
-              isBlog={false}
-              title="Plant AI"
-              description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
-              ghLink="https://github.com/soumyajit4419/Plant_AI"
-              demoLink="https://plant49-ai.herokuapp.com/"
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={suicide}
-              isBlog={false}
-              title="Ai For Social Good"
-              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
-              ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
-              // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={emotion}
-              isBlog={false}
-              title="Face Recognition and Emotion Detection"
-              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
-              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
-              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
-              // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
+              title="Proyecto Integrador"
+              description="As an integrated project, to fulfill the requirements of module 2 of the bootcamp, it was necessary to create an application using the Rick and Morty API. This was applied utilizing all relevant topics"
+              ghLink="https://github.com/Alemar16/Henry-M2_integration_APP"
+              demoLink="https://henry-m2-integration-app.vercel.app/"
             />
           </Col>
         </Row>
